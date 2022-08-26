b99b6a3006aa4401a38255c84aaae73f34f104d9
Luana Mendonça <luamendonca_2000@hotmail.com>
2022-08-26 16:20:01 +0100

    Creating a custom screen component to prevent the content from covering the status bar

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
 import AppText from "./app/components/AppText";
 import AppButton from "./app/components/AppButton";
+import Screen from "./app/components/Screen";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <AppText>Open up App.js to start working on your app!</AppText>
-      <AppButton title="Log In" />
-    </View>
+    <Screen>
+      <View style={styles.container}>
+        <AppText>Open up App.js to start working on your app!</AppText>
+        <AppButton title="Log In" />
+      </View>
+    </Screen>
   );
 }
 
