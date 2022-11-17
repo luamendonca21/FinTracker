1da9fa3f4630caa17be51e709a8600fdbe67da58
luamendonca21 <luamendonca_2000@hotmail.com>
2022-11-17 07:44:14 +0000

    Added index indicator to feature screen (in the future this will be a component reusable)

--- app/screens/FeatureScreen.js
+++ app/screens/FeatureScreen.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { View, StyleSheet, Image } from "react-native";
 import AppText from "../components/AppText";
-import defaultStyles from "../config/colors";
+import defaultStyles from "../config/styles";
 import AppSecondaryButton from "../components/AppSecondaryButton";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
 const FeatureScreen = ({ props }) => {
   return (
     <>
@@ -15,6 +17,43 @@ const FeatureScreen = ({ props }) => {
         <AppText style={styles.subTitle}>
           You can follow your favorite cetaceans in a map and see your location.
         </AppText>
+        <View style={styles.indexContainer}>
+          <MaterialCommunityIcons
+            name="circle"
+            size={12}
+            color={defaultStyles.colors.medium}
+          />
+          <MaterialCommunityIcons
+            name="circle-outline"
+            size={12}
+            color={defaultStyles.colors.medium}
+          />
+          <MaterialCommunityIcons
+            name="circle-outline"
+            size={12}
+            color={defaultStyles.colors.medium}
+          />
+          <MaterialCommunityIcons
+            name="circle-outline"
+            size={12}
+            color={defaultStyles.colors.medium}
+          />
+          <MaterialCommunityIcons
+            name="circle-outline"
+            size={12}
+            color={defaultStyles.colors.medium}
+          />
+          <MaterialCommunityIcons
+            name="circle-outline"
+            size={12}
+            color={defaultStyles.colors.medium}
+          />
+          <MaterialCommunityIcons
+            name="circle-outline"
+            size={12}
+            color={defaultStyles.colors.medium}
+          />
+        </View>
       </View>
       <View style={styles.button}>
         <AppSecondaryButton title="Next" />
@@ -31,19 +70,25 @@ const styles = StyleSheet.create({
   },
   image: {
     borderRadius: 20,
-    marginVertical: 20,
+    marginVertical: 30,
   },
   title: {
     fontSize: 22,
     fontWeight: "bold",
-    color: defaultStyles.primary,
+    color: defaultStyles.colors.primary,
   },
   subTitle: {
     fontSize: 20,
     marginTop: 5,
+    marginBottom: 30,
     textAlign: "center",
     lineHeight: 28,
     width: 260,
+  },
+  indexContainer: {
+    flexDirection: "row",
+    width: 160,
+    justifyContent: "space-evenly",
   },
   button: {
     alignItems: "flex-end",
