683b5e15ed9836be8d441e3a566d1162669c1b60
luamendonca21 <luamendonca_2000@hotmail.com>
2022-11-18 18:52:11 +0000

    fixed keyboard covering the form in register screen and login screen

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import {
+  View,
+  StyleSheet,
+  KeyboardAvoidingView,
+  ScrollView,
+} from "react-native";
 import { WavyHeader } from "../components/Waves";
 import Screen from "../components/Screen";
 import { AppTextInput } from "../components/Inputs";
@@ -9,40 +14,42 @@ import defaultStyles from "../config/styles";
 
 const LoginScreen = (props) => {
   return (
-    <>
-      <WavyHeader
-        customHeight={160}
-        customTop={100}
-        color={defaultStyles.colors.primary}
-        customWavePattern="M0,128L48,117.3C96,107,192,85,288,85.3C384,85,480,107,576,149.3C672,192,768,256,864,277.3C960,299,1056,277,1152,245.3C1248,213,1344,171,1392,149.3L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
-        style={styles.wave}
-      />
-      <Screen>
-        <View style={styles.container}>
-          <AppText style={styles.text}>{`Welcome back, 
+    <KeyboardAvoidingView>
+      <ScrollView>
+        <WavyHeader
+          customHeight={160}
+          customTop={100}
+          color={defaultStyles.colors.primary}
+          customWavePattern="M0,128L48,117.3C96,107,192,85,288,85.3C384,85,480,107,576,149.3C672,192,768,256,864,277.3C960,299,1056,277,1152,245.3C1248,213,1344,171,1392,149.3L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          style={styles.wave}
+        />
+        <Screen>
+          <View style={styles.container}>
+            <AppText style={styles.text}>{`Welcome back, 
 Log In!`}</AppText>
-          <View style={styles.inputsContainer}>
-            <AppTextInput
-              size={25}
-              icon="account-circle"
-              placeholder="Username"
-            />
-            <AppTextInput
-              size={25}
-              icon="lock"
-              placeholder="Password"
-              secureTextEntry
-            />
-            <AppText style={styles.forgotPassword}>
-              Forgot your password?
-            </AppText>
+            <View style={styles.inputsContainer}>
+              <AppTextInput
+                size={25}
+                icon="account-circle"
+                placeholder="Username"
+              />
+              <AppTextInput
+                size={25}
+                icon="lock"
+                placeholder="Password"
+                secureTextEntry
+              />
+              <AppText style={styles.forgotPassword}>
+                Forgot your password?
+              </AppText>
+            </View>
+            <View style={styles.button}>
+              <AppButton title="Log In" />
+            </View>
           </View>
-          <View style={styles.button}>
-            <AppButton title="Log In" />
-          </View>
-        </View>
-      </Screen>
-    </>
+        </Screen>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import {
+  View,
+  StyleSheet,
+  KeyboardAvoidingView,
+  ScrollView,
+} from "react-native";
 import { WavyHeader } from "../components/Waves";
 import Screen from "../components/Screen";
 import { AppTextInput } from "../components/Inputs";
@@ -9,39 +14,41 @@ import defaultStyles from "../config/styles";
 
 const LoginScreen = (props) => {
   return (
-    <>
-      <WavyHeader
-        customHeight={160}
-        customTop={100}
-        color={defaultStyles.colors.primary}
-        customWavePattern="M0,128L48,117.3C96,107,192,85,288,85.3C384,85,480,107,576,149.3C672,192,768,256,864,277.3C960,299,1056,277,1152,245.3C1248,213,1344,171,1392,149.3L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
-        style={styles.wave}
-      />
-      <Screen>
-        <View style={styles.container}>
-          <AppText style={styles.text}>{`Hello, 
+    <KeyboardAvoidingView>
+      <ScrollView>
+        <WavyHeader
+          customHeight={160}
+          customTop={100}
+          color={defaultStyles.colors.primary}
+          customWavePattern="M0,128L48,117.3C96,107,192,85,288,85.3C384,85,480,107,576,149.3C672,192,768,256,864,277.3C960,299,1056,277,1152,245.3C1248,213,1344,171,1392,149.3L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          style={styles.wave}
+        />
+        <Screen>
+          <View style={styles.container}>
+            <AppText style={styles.text}>{`Hello, 
 Sign Up!`}</AppText>
-          <View style={styles.inputsContainer}>
-            <AppTextInput
-              size={25}
-              icon="account-circle"
-              placeholder="Username"
-            />
-            <AppTextInput size={25} icon="email" placeholder="Email" />
+            <View style={styles.inputsContainer}>
+              <AppTextInput
+                size={25}
+                icon="account-circle"
+                placeholder="Username"
+              />
+              <AppTextInput size={25} icon="email" placeholder="Email" />
 
-            <AppTextInput
-              size={25}
-              icon="lock"
-              placeholder="Password"
-              secureTextEntry
-            />
+              <AppTextInput
+                size={25}
+                icon="lock"
+                placeholder="Password"
+                secureTextEntry
+              />
+            </View>
+            <View style={styles.button}>
+              <AppButton title="Sign Up" />
+            </View>
           </View>
-          <View style={styles.button}>
-            <AppButton title="Sign Up" />
-          </View>
-        </View>
-      </Screen>
-    </>
+        </Screen>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
