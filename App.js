1b7d10e7f1dc8d843106345c90d7ec156dad1652
luamendonca21 <luamendonca_2000@hotmail.com>
2023-02-26 20:17:16 +0000

    fixed previous screen flickering when we get the user state when the app reloads

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useCallback, useState } from "react";
 import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-
+import * as SplashScreen from "expo-splash-screen";
 import AppNavigator from "./app/navigation/AppNavigator";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 
@@ -10,8 +10,11 @@ import myTheme from "./app/navigation/navigationTheme";
 import AuthContext from "./app/auth/context";
 import authStorage from "./app/auth/storage";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
   const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
 
   const restoreUser = async () => {
     const user = await authStorage.getUser();
@@ -19,8 +22,40 @@ export default function App() {
   };
 
   useEffect(() => {
+    async function restoreUser() {
+      try {
+        const user = await authStorage.getUser();
+        if (user) {
+          setUser(user);
+        }
+      } catch (error) {
+        console.warn(error);
+      } finally {
+        setIsReady(true);
+        SplashScreen.hideAsync();
+      }
+    }
     restoreUser();
-  });
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (isReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
+
+  if (!isReady) {
+    return null;
+  }
   return (
     <>
       <StatusBar
@@ -29,7 +64,11 @@ export default function App() {
       />
       <AuthContext.Provider value={{ user, setUser }}>
         <NavigationContainer theme={myTheme}>
-          {user ? <AppNavigator /> : <AuthNavigator />}
+          {user ? (
+            <AppNavigator onLayout={onLayoutRootView} />
+          ) : (
+            <AuthNavigator onLayout={onLayoutRootView} />
+          )}
         </NavigationContainer>
       </AuthContext.Provider>
     </>
