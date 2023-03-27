a72c6e3a66934851f209f292fd4f93f299f68f25
luamendonca21 <luamendonca_2000@hotmail.com>
2023-03-27 01:49:22 +0100

    nothing special

--- app/config/settings.js
+++ app/config/settings.js
@@ -14,7 +14,7 @@ const settings = {
 const getCurrentSettings = () => {
   if (__DEV__) return settings.dev;
   if (Constants.manifest.releaseChannel === "staging") return settings.staging;
-  return settings.prof;
+  return settings.prod;
 };
 
 export default getCurrentSettings();
