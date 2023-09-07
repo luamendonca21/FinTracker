3ba81e9ca5a09e26c296587b6493ed555ceca310
luamendonca21 <luamendonca_2000@hotmail.com>
2023-09-07 17:18:05 +0100

    added an alert to inform the user that the notifications features  is not available

--- app/components/Lists/ListSettings.js
+++ app/components/Lists/ListSettings.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, FlatList } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
+import { Alert } from "../Alerts";
 import { ListItem, ListItemSeparator } from "../../components/Lists";
 import Icon from "../../components/Icon";
 import useAuth from "../../auth/useAuth";
@@ -9,16 +10,28 @@ import useAuth from "../../auth/useAuth";
 import defaultStyles from "../../config/styles";
 
 const ListSettings = ({ menuItems }) => {
+  // ------ STATE MANAGEMENT -------
+  const [isAlertVisible, setIsAlertVisible] = useState(false);
+
   const { logOut } = useAuth();
   const navigation = useNavigation();
+
+  // ------- UTILS -------------
+
+  const showAlert = () => setIsAlertVisible(true);
+
+  const hideAlert = () => setIsAlertVisible(false);
 
   const handlePress = (item) => {
     if (item.icon.name == "logout") {
       logOut();
+    } else if (item.target == "") {
+      showAlert(true);
     } else {
       navigation.navigate(item.target);
     }
   };
+
   const renderItem = ({ item }) => (
     <ListItem
       onPress={() => handlePress(item)}
@@ -48,6 +61,16 @@ const ListSettings = ({ menuItems }) => {
         keyExtractor={(item) => item.id}
         renderItem={renderItem}
         ItemSeparatorComponent={ListItemSeparator}
+      />
+      <Alert
+        showAlert={isAlertVisible}
+        msg="Esta funcionalidade ainda não está disponível."
+        showConfirmButton
+        confirmText="Ok"
+        confirmButtonColor={defaultStyles.colors.secondary}
+        onConfirm={() => {
+          hideAlert();
+        }}
       />
     </View>
   );
